5c9e12e49914c4fab6578710dbbaece4b8dd2010
Emmanuel <chizaramchibuikem@gmail.com>
2026-04-14 13:05:56 +0100

    email fixing phase two

--- confcore/settings.py
+++ confcore/settings.py
@@ -113,9 +113,9 @@ EMAIL_PORT = 2525
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 30
-EMAIL_HOST_USER = 'chizaramchibuikem@gmail.com'  # The login from Brevo
+EMAIL_HOST_USER = 'a7f23a001@smtp-brevo.com'  # The login from Brevo
 EMAIL_HOST_PASSWORD = os.getenv('BREVO_PASSWORD') # The key you just generated
-DEFAULT_FROM_EMAIL = 'chizaramchibuikem@gmail.com'
+DEFAULT_FROM_EMAIL = 'the-confy <chizaramchibuikem@gmail.com>'
 
 
 # Password validation
